83fa5f364dc03f7261069c3081b6ebf5557dcaf2
lucascr <lcrodrigues1995@gmail.com>
2022-11-18 17:42:33 -0300

    Update redirect to after group creation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,7 +26,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully created.' }
+        format.html { redirect_to groups_path, notice: 'Group was successfully created.' }
         format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new, status: :unprocessable_entity }
