420af4bba8795022c882a7b991a78b88d2ac3f58
lucascr <lcrodrigues1995@gmail.com>
2022-11-18 18:51:33 -0300

    Fix linters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,10 +15,6 @@ RSpec.describe User, type: :model do
       expect(subject).not_to be_valid
     end
 
-    it 'Name length should be lesser than 15 characters' do
-      expect(subject.name).to satisfy { |n| n.length <= 27 }
-    end
-
     it 'email should return a string' do
       expect(subject.email).to be_a_kind_of(String)
     end
